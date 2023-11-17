dc5c7c3f6a700d371c023d2e6a657dce10521bed
Sangyao42 <wangsangyao@hotmail.com>
2023-11-17 19:25:12 +0100

    Cleaned[ex00]: deleted comments in class ScalerConverter

--- ex00/include/ScalerConverter.hpp
+++ ex00/include/ScalerConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:32:20 by sawang            #+#    #+#             */
-/*   Updated: 2023/11/17 16:32:55 by sawang           ###   ########.fr       */
+/*   Updated: 2023/11/17 19:24:42 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,6 @@
 #include <iostream>
 #include <string>
 #include <limits>
-
-// #define WHITE_SPACE " \t\n\v\f\r"
-
-// enum	eType
-// {
-// 	PSEUDO,
-// 	CHAR,
-// 	INT,
-// 	FLOAT,
-// 	DOUBLE,
-// 	UNKNOWN
-// };
 
 class ScalerConverter
 {
@@ -39,34 +27,4 @@ class ScalerConverter
 		ScalerConverter(ScalerConverter const &cpy);
 		ScalerConverter &operator=(ScalerConverter const &rhs);
 		~ScalerConverter();
-
-		// ScalerConverter(std::string str);
-
-		// //parser
-		// static eType	_parseInput(std::string str);
-		// static void	_trimWhiteSpace(std::string &str);
-		// static bool	_parsePseudo(std::string str);
-		// static bool	_parseChar(std::string str);
-		// static bool	_parseInt(std::string str);
-		// static bool	_parseFloat(std::string str);
-		// static bool	_parseDouble(std::string str);
-
-		// //convert and print
-		// static void	_convertPseudo(void);
-		// //operator overload
-		// operator char() const;
-		// operator int() const;
-		// operator float() const;
-		// operator double() const;
-
-		// //exception: in private or public?
-		// class InputInvalidException : public std::exception
-		// {
-		// 	virtual const char* what() const throw();
-		// };
-
-		// //member variables;
-		// static  std::string	_input;
-		// static eType	 	_inputType;
-		// static Input _input;
 };
